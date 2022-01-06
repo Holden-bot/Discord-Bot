0598ea332bf373e580ab02ceeccd37359726829b
Matthew Gurski <mkg061@usask.ca>
2022-01-06 14:19:57 -0600

    Added event folder, with simple event files and dynamic event register

--- src/index.js
+++ src/index.js
@@ -6,22 +6,31 @@ const { token } = require('../config.json');
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+// retrieve all event files 
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// retrieve all command files
 client.commands = new Collection();
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     // Set a new item in the Collection
     // With the key as the command name and the value as the exported module
     client.commands.set(command.data.name, command);
 }
-
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Ready!');
-});
-
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
@@ -37,6 +46,7 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
+
 
 // Login to Discord with your client's token
 client.login(token);
